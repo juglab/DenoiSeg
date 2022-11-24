21cdac3b73ee10dbe5337444b2bd27dc89102fcc
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2022-11-24 16:15:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,8 +31,10 @@ setup(name='denoiseg',
           'Topic :: Scientific/Engineering',
           'License :: OSI Approved :: BSD License',
 
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
       ],
 
       install_requires=[
